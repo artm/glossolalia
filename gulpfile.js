f07731a0c0b9c2417bf46823ad90349e2866db97
Artem Baguinski <abaguinski@depraktijkindex.nl>
2016-01-19 16:37:48 +0100

    webpack config

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var webpack = require('webpack-stream');
 
 gulp.task('default', function() {
-  return gulp.src('src/js/index.js')
-  .pipe(webpack())
-  .pipe(gulp.dest('dist/'));
+  return gulp.src('./src/js/**/*.js')
+  .pipe(webpack(require('./config/webpack.config.js')))
+  .pipe(gulp.dest('./dist/js'));
 });
