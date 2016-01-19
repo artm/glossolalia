7169148963315469b1c54de68d5320882719bf62
Artem Baguinski <abaguinski@depraktijkindex.nl>
2016-01-19 16:58:39 +0100

    copy static files over to dist

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,23 @@
 var gulp = require('gulp');
 var webpack = require('webpack-stream');
 var del = require('del');
+var copy = require('gulp-copy');
 
 gulp.task('clean:dist', function() {
   return del(['dist/**/*']);
 });
 
 gulp.task('webpack', function() {
-  return gulp.src('./src/js/**/*.js')
-  .pipe(webpack(require('./config/webpack.config.js')))
-  .pipe(gulp.dest('./dist/js'));
+  return gulp
+    .src('./src/js/**/*.js')
+    .pipe(webpack(require('./config/webpack.config.js')))
+    .pipe(gulp.dest('./dist/js'));
 });
 
-gulp.task('default', ['clean:dist', 'webpack']);
+gulp.task('static:copy', function() {
+  return gulp
+    .src('./src/static/**/*')
+    .pipe(copy('./dist/', {prefix: 2}));
+});
+
+gulp.task('default', ['clean:dist', 'webpack', 'static:copy']);
