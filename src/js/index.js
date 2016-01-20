c5c55fbbdfbdefe4725646a8fb9aee50b7b9b91e
Artem Baguinski <abaguinski@depraktijkindex.nl>
2016-01-20 06:52:42 +0100

    figuring out google drive api

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,33 @@
-console.log('hi there');
+
+// Load the Realtime API, no auth needed.
+window.gapi.load('auth:client,drive-realtime,drive-share', start);
+
+function start() {
+  var doc = new GlossolaliaDoc();
+  console.log(doc.model.toJson());
+}
+
+function GlossolaliaDoc() {
+  this.doc = gapi.drive.realtime.newInMemoryDocument();
+  this.model = this.doc.getModel();
+  this.streams = this.model.createList();
+  this.model.getRoot().set('streams', this.streams);
+
+  var masterStream = this.createStream();
+  this.streams.push(masterStream);
+  masterStream.appendFragment("Here be content");
+}
+
+GlossolaliaDoc.prototype.createStream = function() {
+  var doc = this;
+  var stream = this.model.createMap({ title: 'new text', language: 'en' });
+  var fragments = this.model.createList();
+  stream.set('fragments', fragments);
+
+  stream.appendFragment = function(text) {
+    text = doc.model.createString(text);
+    fragments.push(text);
+  }
+
+  return stream;
+}
