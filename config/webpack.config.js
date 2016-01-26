a01108eef9c4c5f9b2d3424f99aaa50f1903974b
Artem Baguinski <abaguinski@depraktijkindex.nl>
2016-01-26 06:45:57 +0100

    add riot/babel dependencies

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -4,5 +4,19 @@ module.exports = {
   },
   output: {
     filename: '[name].js',
-  }
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      // makes global riot require riot library
+      riot: 'riot'
+    })
+  ],
+  module: {
+    preLoaders: [
+      { test: /\.tag$/, exclude: /node_modules/, loader: 'riotjs-loader', query: { type: 'none' } }
+    ],
+    loaders: [
+      { test: /\.js$|\.tag$/, exclude: /node_modules/, loader: 'babel-loader' }
+    ]
+  },
 };
